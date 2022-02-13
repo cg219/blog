43c334f67003eabb0b160538b01b9e31d835f66f
Clemente <dev@imkreative.com>
2022-02-12 20:21:59 -0500

    Fix errors

--- src/components/post/component.js
+++ src/components/post/component.js
@@ -5,6 +5,8 @@ import styles from "./style";
 const Post = (props) => {
     const feature = props.feature_image || "https://storage.googleapis.com/kreativemente-assets/uploads/default2.jpg";
 
+    console.log(props);
+
     return (
         <section className={styles.Post}>
             <Link to={`/read/${props.slug}`} />
@@ -14,8 +16,8 @@ const Post = (props) => {
             <time className={styles.Date}>{props.formatted_date}</time>
             <h1 className={styles.Title}>{props.title}</h1>
             <section className={styles.Author}>
-                <div className={styles.AuthorImage} style={{ backgroundImage: `url(${props.primary_author.profile_image})` }}></div>
-                <span className={styles.AuthorName}>{props.primary_author.name}</span>
+                <div className={styles.AuthorImage} style={{ backgroundImage: `url(${props.primary_author?.profile_image})` }}></div>
+                <span className={styles.AuthorName}>{props.primary_author?.name}</span>
             </section>
             <p className={styles.Excerpt}>{props.custom_excerpt}</p>
         </section>
