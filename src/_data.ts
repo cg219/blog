f1da64deaf4345b04beb4a35debb48f07ad4f14c
Clemente <dev@imkreative.com>
2022-06-26 18:23:38 -0400

    Update post data

--- src/_data.ts
+++ src/_data.ts
@@ -2,7 +2,7 @@ const API_URL = Deno.env.get('IMK_API_URL');
 const API_KEY = Deno.env.get('IMK_API_KEY');
 const API_VERSION = 'v4';
 
-const res = await fetch(`${API_URL}/ghost/api/content/posts/?key=${API_KEY}`);
+const res = await fetch(`${API_URL}/ghost/api/content/posts/?key=${API_KEY}&include=tags,authors`);
 const data = await res.json();
 
 export const { posts } = data;
--- src/posts.tmpl.ts
+++ src/posts.tmpl.ts
@@ -2,10 +2,21 @@ export const layout = 'layouts/base.tmpl.ts';
 
 export default function* ({ posts }) {
     for (const post of posts) {
+        console.log(post)
+        const date = new Date(post.published_at);
+
         yield {
             url: `/read/${post.slug}`,
             title: post.title,
-            content: `<div class="content single-post">${post.html}</div>`
+            content: `
+            <div class="content">
+                <h1 class="post-title">${post.title}</h1>
+                <h3 class="post-date">${new Date(post.published_at)}</h3>
+                <h3 class="post-author">${post.primary_author.name}</h3>
+                <div class="single-post">
+                    ${post.html}
+                </div>
+            </div>`
         }
     }
 }
